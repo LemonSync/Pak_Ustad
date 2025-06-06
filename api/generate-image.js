7a17bfc7c1f42ad163610ec622914c27e3e3e5a2
LemonSync <grouplemon0@gmail.com>
2025-06-06 21:41:07 +0700

    Update generate-image.js

--- api/generate-image.js
+++ api/generate-image.js
@@ -74,7 +74,7 @@ module.exports = async (req, res) => {
 
     ctx.fillStyle = '#000000';
     ctx.font = `bold 37px 'default'`;
-    ctx.fillText(isi, centerX, 200);
+    ctx.fillText(isi, centerX, 180);
 
     const output = canvas.toBuffer('image/png');
     res.setHeader('Content-Type', 'image/png');
